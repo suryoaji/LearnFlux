5e2d86dcea6790518daa3dcdecd8245abb5653d6
Martin Tjandra <martin.tjandra@idesolusiasia.com>
2016-05-10 13:22:12 +0700

    Further change the home to give four different options of the mosaic design

--- iOS/LearnFlux/Home.swift
+++ iOS/LearnFlux/Home.swift
@@ -63,29 +63,39 @@ class Home: UICollectionViewController {
             NSLog("view = \(a)");
         }
         let bg = cell.viewWithTag(10) as! UIImageView;
+        let layer = cell.viewWithTag(12)!;
         let icon = cell.viewWithTag(11) as! UIImageView;
         let label = cell.viewWithTag(21) as! UILabel;
         NSLog("label = \(label.text), x = \(label.x), y = \(label.y), w = \(label.width), h = \(label.height)");
+        label.x = 14; label.y = 111;
+        label.textAlignment = .Center;
         cell.bringSubviewToFront(label);
         if (indexPath.row == 0) {
             bg.image = UIImage(named: "mozaic_organization.png");
             icon.image = UIImage(named: "globe_white-512.png");
+            layer.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.6);
             label.text = "Organization";
         }
         else if (indexPath.row == 1) {
             bg.image = UIImage(named: "mozaic_activities.png");
             icon.image = UIImage(named: "headphones_white-512.png");
+            layer.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.8);
             label.text = "Activities";
         }
         else if (indexPath.row == 2) {
             bg.image = UIImage(named: "mozaic_settings.png");
             icon.image = UIImage(named: "wrench_white-512.png");
+            layer.backgroundColor = UIColor.init(red: 0, green: 144/255, blue: 1, alpha: 1);
             label.text = "Settings";
         }
         else if (indexPath.row == 3) {
             bg.image = UIImage(named: "mozaic_chat.png");
-            icon.image = UIImage(named: "speech_bubble_white-512.png");
+            icon.image = UIImage(named: "speech_bubble_white2-512.png");
+            icon.center = bg.center;
+            layer.backgroundColor = UIColor.init(red: 1, green: 0, blue: 0, alpha: 1);
             label.text = "Chats";
+            label.x = 10; label.y = cell.height - label.height - 30;
+            label.textAlignment = .Left
         }
         cell.sendSubviewToBack(bg)
 
