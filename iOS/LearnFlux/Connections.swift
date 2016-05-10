b4be3396eaf816891df18230013fffeba5169274
Martin Tjandra <martin.tjandra@idesolusiasia.com>
2016-05-10 13:24:24 +0700

    Add data mockup for Connections screen

--- iOS/LearnFlux/Connections.swift
+++ iOS/LearnFlux/Connections.swift
@@ -18,10 +18,39 @@ class Connections : UITableViewController {
         self.navigationItem.leftBarButtonItem = item;
         item.action = #selector(self.revealMenu);
         item.target = self;
+        
+        self.tabBarController?.title = "Connections";
     }
     
     @IBAction func revealMenu (sender: AnyObject) {
         self.revealController.showViewController(self.revealController.leftViewController);
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2;
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch (section) {
+        case 1: return 3;
+        default: return 0;
+        }
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return tableView.dequeueReusableCellWithIdentifier(indexPath.code)!.height;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(indexPath.code)!;
+        cell.setSeparatorType(CellSeparatorFull);
+        let img = cell.viewWithTag(10) as! UIImageView;
+        img.makeRounded();
+        return cell;
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: false);
     }
 
 }
