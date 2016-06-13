a795ce018d99dc4f3c3e5cf39a6767ca66f20a0c
Martin Tjandra <martin.tjandra@idesolusiasia.com>
2016-06-13 10:37:55 +0700

    Install pod AZDropdownMenu

--- iOS/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ iOS/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,6 +84,7 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/AZDropdownMenu.framework"
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/CZPicker.framework"
   install_framework "Pods/JSQMessagesViewController.framework"
@@ -92,6 +93,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/RAReorderableLayout.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/AZDropdownMenu.framework"
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/CZPicker.framework"
   install_framework "Pods/JSQMessagesViewController.framework"
