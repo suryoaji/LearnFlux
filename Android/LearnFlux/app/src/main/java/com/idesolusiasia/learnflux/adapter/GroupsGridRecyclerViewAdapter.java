f84e05351ff67d0ab36a2f19c53ae69c56a4f88c
Herprasetyo <yudith.octo@idesolusiasia.com>
2016-08-12 10:59:00 +0700

    reconstruct GroupsGridRecyclerViewAdapter

--- Android/LearnFlux/app/src/main/java/com/idesolusiasia/learnflux/adapter/GroupsGridRecyclerViewAdapter.java
+++ Android/LearnFlux/app/src/main/java/com/idesolusiasia/learnflux/adapter/GroupsGridRecyclerViewAdapter.java
@@ -16,24 +16,24 @@ import com.idesolusiasia.learnflux.R;
 import com.idesolusiasia.learnflux.entity.Group;
 import com.idesolusiasia.learnflux.util.Functions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by NAIT ADMIN on 01/07/2016.
  */
 
-public class GroupsGridRecyclerViewAdapter extends RecyclerView.Adapter<GroupsTileHolder> {
-	private List<Group> groups;
+public class GroupsGridRecyclerViewAdapter extends RecyclerView.Adapter<GroupsGridRecyclerViewAdapter.GroupsTileHolder> {
+	ArrayList<Group> groups = new ArrayList<>();
 	private Context context;
 
-	public GroupsGridRecyclerViewAdapter(Context context, List<Group> itemList) {
+
+	public GroupsGridRecyclerViewAdapter(Context context, ArrayList<Group> itemList) {
 		this.groups = itemList;
 		this.context = context;
 	}
-
 	@Override
 	public GroupsTileHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
 		View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_groups, null);
 		GroupsTileHolder rcv = new GroupsTileHolder(layoutView);
 		return rcv;
@@ -55,28 +55,25 @@ public class GroupsGridRecyclerViewAdapter extends RecyclerView.Adapter<GroupsTi
 	public int getItemCount() {
 		return this.groups.size();
 	}
-}
-class GroupsTileHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-	public TextView tvGroupName;
-	public LinearLayout layout;
 
-	public GroupsTileHolder(View itemView) {
-		super(itemView);
-		itemView.setOnClickListener(this);
-		tvGroupName = (TextView) itemView.findViewById(R.id.tvGroupName);
-		layout = (LinearLayout) itemView.findViewById(R.id.layoutGroupsGrid);
+	public class GroupsTileHolder extends RecyclerView.ViewHolder {
+		public TextView tvGroupName;
+		public LinearLayout layout;
 
-	}
-
-	@Override
-	public void onClick(View view) {
-		Toast.makeText(view.getContext(), tvGroupName.getText() + " clicked", Toast.LENGTH_SHORT).show();
-		Intent i = new Intent(view.getContext(), GroupDetailActivity.class);
-		view.getContext().startActivity(i);
-		//Toast.makeText(view.getContext(), "Clicked Position = " + getAdapterPosition(), Toast.LENGTH_SHORT).show();
-		/*if (text.getText().toString().equalsIgnoreCase("chat")){
-			Intent i = new Intent(view.getContext(),ChatsActivity.class);
-			view.getContext().startActivity(i);
-		}*/
+		public GroupsTileHolder(View itemView) {
+			super(itemView);
+			tvGroupName = (TextView) itemView.findViewById(R.id.tvGroupName);
+			layout = (LinearLayout) itemView.findViewById(R.id.layoutGroupsGrid);
+			itemView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					final int pos = getAdapterPosition();
+					Toast.makeText(view.getContext(), tvGroupName.getText() + " clicked", Toast.LENGTH_SHORT).show();
+					Intent i = new Intent(view.getContext(), GroupDetailActivity.class);
+					i.putExtra("title", groups.get(pos).getName());
+					view.getContext().startActivity(i);
+				}
+			});
+		}
 	}
 }
