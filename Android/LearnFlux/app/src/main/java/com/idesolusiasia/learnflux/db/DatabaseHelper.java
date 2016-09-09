62a158cf44a5da01f77c2b7173892739ec6bd04b
Agatha Cynthia <agatha.cynthia@idesolusiasia.com>
2016-09-09 17:01:47 +0700

    turn on foreign key pragma

--- Android/LearnFlux/app/src/main/java/com/idesolusiasia/learnflux/db/DatabaseHelper.java
+++ Android/LearnFlux/app/src/main/java/com/idesolusiasia/learnflux/db/DatabaseHelper.java
@@ -105,6 +105,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	}
 
 	@Override
+	public void onOpen(SQLiteDatabase db) {
+		super.onOpen(db);
+		if (!db.isReadOnly()) {
+			// Enable foreign key constraints
+			db.execSQL("PRAGMA foreign_keys=ON;");
+		}
+	}
+
+	@Override
 	public void onCreate(SQLiteDatabase db) {
 
 		db.execSQL(DATABASE_CREATE_THREAD);
